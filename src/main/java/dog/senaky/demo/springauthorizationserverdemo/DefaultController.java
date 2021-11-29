8e3ea74139582f0dc67afffd883ce7a883ce5654
fuzhou.dong <sneakydog@yeah.net>
2021-11-29 19:25:37 +0800

    tests

--- src/main/java/dog/senaky/demo/springauthorizationserverdemo/DefaultController.java
+++ src/main/java/dog/senaky/demo/springauthorizationserverdemo/DefaultController.java
@@ -1,18 +1,28 @@
 package dog.senaky.demo.springauthorizationserverdemo;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class IndexController {
+@Controller
+public final class DefaultController {
+
+    @ResponseBody
     @GetMapping({"/", "/index", "/index.html", ""})
     public String index(){
         return "index";
     }
 
+    @ResponseBody
     @GetMapping("/favicon.ico")
     public String faviconico(){
         return "favicon.ico";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
     }
 }
